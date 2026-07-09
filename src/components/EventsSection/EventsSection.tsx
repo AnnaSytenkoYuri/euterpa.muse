b11dd2bdb5f23f5f650b03aa433ab9e43fa91081
Anna Sytenko <anna_sytenko91@icloud.com>
2026-07-09 18:54:10 +0700

    complited addaptive design for projects section, added annimation. Starting to add adaptive design for events section.

--- src/components/EventsSection/EventsSection.tsx
+++ src/components/EventsSection/EventsSection.tsx
@@ -1,8 +1,63 @@
+import css from "./EventsSection.module.css";
+
 export default function EventsSection() {
-    return(
-        <section className="events-section">
-            <h2>Events Section</h2>
-            <p>This is the events section of the page.</p>
-        </section>
-    )
+  return (
+    <section className={css.eventSection}>
+      <div className="container">
+        <p className={css.subTextEvent}>concerts</p>
+        <h2 className={css.eventTitle}>Upcoming events</h2>
+        <ul className={css.eventsList}>
+          <li className={css.eventItem}>
+            <div className={css.eventDate}>
+              <span className={css.eventDay}>25</span>
+              <span className={css.eventMonth}>Sep</span>
+            </div>
+            <div className={css.eventInfo}>
+              <h3 className={css.cardTitle}>Band At Work</h3>
+              <p className={css.cardText}>Ebern. Strasse 546</p>
+              <span className={css.cardDetails}>Start at 20:30</span>
+            </div>
+            <button className={css.ticketBtn}>Tickets</button>
+          </li>
+          <li className={css.eventItem}>
+            <div className={css.eventDate}>
+              <span className={css.eventDay}>25</span>
+              <span className={css.eventMonth}>Sep</span>
+            </div>
+            <div className={css.eventInfo}>
+              <h3 className={css.cardTitle}>Band At Work</h3>
+              <p className={css.cardText}>Ebern. Strasse 546</p>
+              <span className={css.cardDetails}>Start at 20:30</span>
+            </div>
+            <button className={css.ticketBtn}>Tickets</button>
+          </li>
+          <li className={css.eventItem}>
+            <div className={css.eventDate}>
+              <span className={css.eventDay}>25</span>
+              <span className={css.eventMonth}>Sep</span>
+            </div>
+            <div className={css.eventInfo}>
+              <h3 className={css.cardTitle}>Band At Work</h3>
+              <p className={css.cardText}>Ebern. Strasse 546</p>
+              <span className={css.cardDetails}>Start at 20:30</span>
+            </div>
+            <button className={css.ticketBtn}>Tickets</button>
+          </li>
+          <li className={css.eventItem}>
+            <div className={css.eventDate}>
+              <span className={css.eventDay}>25</span>
+              <span className={css.eventMonth}>Sep</span>
+            </div>
+            <div className={css.eventInfo}>
+              <h3 className={css.cardTitle}>Band At Work</h3>
+              <p className={css.cardText}>Ebern. Strasse 546</p>
+              <span className={css.cardDetails}>Start at 20:30</span>
+            </div>
+            <button className={css.ticketBtn}>Tickets</button>
+          </li>
+        </ul>
+        <button className={css.checkBtn}>Check all</button>
+      </div>
+    </section>
+  );
 }
--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -13,7 +13,7 @@ export default function ProjectsSection() {
                 src="/images/band-at-work1.png"
                 alt="Project"
                 width={260}
-                height={212} 
+                height={212}
                 className={css.projectImage}
               />
               <div className={css.overlay}>
@@ -21,7 +21,80 @@ export default function ProjectsSection() {
                 <p className={css.cardCategory}>Alternative Pop | Indie</p>
                 <button className={css.learnBtn}>
                   <span>Learn More</span>
-                  <svg>...</svg>
+                  <svg
+                    width="12"
+                    height="10"
+                    viewBox="0 0 12 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M7.1 9.5L11.5 5L7.1 0.5M11.5 5H0.5"
+                      stroke="white"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className={css.projectCard}>
+              <Image
+                src="/images/TatYana1.png"
+                alt="Project"
+                width={260}
+                height={212}
+                className={css.projectImage}
+              />
+              <div className={css.overlay}>
+                <h3 className={css.cardTitle}>TatYANa</h3>
+                <p className={css.cardCategory}>Alternative Pop | Indie</p>
+                <button className={css.learnBtn}>
+                  <span>Learn More</span>
+                  <svg
+                    width="12"
+                    height="10"
+                    viewBox="0 0 12 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M7.1 9.5L11.5 5L7.1 0.5M11.5 5H0.5"
+                      stroke="white"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className={css.projectCard}>
+              <Image
+                src="/images/hardy1.png"
+                alt="Project"
+                width={260}
+                height={212}
+                className={css.projectImage}
+              />
+              <div className={css.overlay}>
+                <h3 className={css.cardTitle}>Hardy</h3>
+                <p className={css.cardCategory}>Alternative Pop | Indie</p>
+                <button className={css.learnBtn}>
+                  <span>Learn More</span>
+                  <svg
+                    width="12"
+                    height="10"
+                    viewBox="0 0 12 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M7.1 9.5L11.5 5L7.1 0.5M11.5 5H0.5"
+                      stroke="white"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
                 </button>
               </div>
             </div>
