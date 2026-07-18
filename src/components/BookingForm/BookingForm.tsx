f10558ede3080f4b3494c47a7849f4a0e680f6b3
Anna Sytenko <anna_sytenko91@icloud.com>
2026-07-18 17:23:50 +0700

    adding styles to BookingForm not complited

--- src/components/BookingForm/BookingForm.tsx
+++ src/components/BookingForm/BookingForm.tsx
@@ -1,35 +1,57 @@
-'use client';
-import css from './BookingForm.module.css'
+"use client";
+import css from "./BookingForm.module.css";
 import { useEffect, useRef } from "react";
 
+export default function BookingForm() {
+  const nameInputRef = useRef<HTMLInputElement>(null);
 
-export default function BookingForm(){
-   
-    const nameInputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    nameInputRef.current?.focus();
+  }, []);
 
-    useEffect(()=>{
-        nameInputRef.current?.focus();
-    },[])
+  return (
+    <div className={css.bookingForm}>
+      <header className={css.header}>
+        <h2 id="booking-modal-title" className={css.title}>
+          Book a lesson
+        </h2>
+        <p className={css.description}>Fill out the form, and I will contact you to confirm.</p>
+      </header>
 
-    return(
-        <form className={css.form}>
-            <h2 id="booking-modal-title" className={css.title}>Book a lesson</h2>
-            <p>Fill out the form, and I will contact you to confirm.</p>
-            <div>
-                <input type="text" ref={nameInputRef} placeholder='Your name' />
-                <input type="email" placeholder='Email' />
-                <input type="phone" placeholder='Phone' />
-            </div>
-            <label htmlFor=""></label>
-            <select name="" id=""></select>
-            <label htmlFor=""></label>
-            <select name="" id=""></select>
-            <label htmlFor=""></label>
-            {/* <DatePIcker/> */}
-            <label htmlFor=""></label>
-            <textarea name="" id=""></textarea>
-            <button>book now</button>
-            <p>By clicking `&quot`Book now`&quot` you agree to our Privacy Policy.</p>
-        </form>
-    )
+      <form className={css.form}>
+        <div className={css.inputGroup}>
+          <input type="text" ref={nameInputRef} placeholder="Your name" className={css.input}/>
+          <input type="email" placeholder="Email" className={css.input}/>
+          <input type="phone" placeholder="Phone" className={css.input}/>
+        </div>
+
+        <div className={css.field}>
+          <label htmlFor="lesonFormat">Lesson format</label>
+          {/* React Select */}
+        </div>
+
+        <div className={css.field}>
+          <label htmlFor="date">Date and Time</label>
+          {/* <DatePIcker/> */}
+        </div>
+
+        <div className={css.field}>
+          <label htmlFor="level">Your vocal level</label>
+          {/* React Select */}
+        </div>
+
+        <div className={css.field}>
+          <label htmlFor="message">
+            Leave your message <span>optional</span>
+          </label>
+          <textarea className={css.textarea} placeholder="You can write a message or share your wishes, if you'd like."/>
+        </div>
+
+        <button className={css.bookBtn}>book now</button>
+      </form>
+        <p className={css.policy}>
+        By clicking &quot;Book now&quot; you agree to our Privacy Policy.
+        </p>
+    </div>
+  );
 }
